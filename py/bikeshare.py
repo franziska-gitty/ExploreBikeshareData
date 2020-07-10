d9457a48f5ecb05c356904c6f63d7f82f39e5274
Franziska-Koertke <franziskakoertke@gmx.de>
2020-07-10 09:45:32 +0200

    Add restart documentation line into bikeshare.py

--- py/bikeshare.py
+++ py/bikeshare.py
@@ -221,6 +221,7 @@ def main():
         trip_duration_stats(df, month, day)
         user_stats(df, month, day, city)
 
+        # restart query or not
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
